6c2a95b682b538a10d9fcf8b4f7499dc904f62d8
Re1o <reio.reimers@hotmail.com>
2020-04-06 16:06:47 +0300

    4.6.20

---
+++ EstonianData/ESTdata.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EstonianData
+{
+    class ESTdata
+    {
+        public string Status { get; set; }
+        public Results Results { get; set; }
+        public bool Value { get;  set; }
+    }
+    public class Results
+    {
+        public string name { get; set; }
+        public string cioc { get; set; }
+        public string domain { get; set; }
+        public string capital { get; set; }
+        public string region { get; set; }
+        public string population { get; set; }
+        public string language { get; set; }
+    }
+}
+
+
+/*
+Display:
+name
+cioc
+domain
+capital
+region
+population
+language
+*/
---
+++ EstonianData/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Net;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace EstonianData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userDataUrl = "https://restcountries.eu/rest/v2/name/eesti";
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(userDataUrl);
+            request.Method = "GET";
+
+            var webResponse = request.GetResponse();
+            var webStream = webResponse.GetResponseStream();
+
+            using (var responseReader = new StreamReader(webStream))
+            {
+                var response = responseReader.ReadToEnd();
+                ESTdata eSTdata = JsonConvert.DeserializeObject<ESTdata>(response);
+                Console.WriteLine(eSTdata.Value);
+                Console.WriteLine(eSTdata.Status);
+                Console.WriteLine($"name: {eSTdata.Results.name}");
+                Console.WriteLine($"cioc: {eSTdata.Results.cioc}");
+                Console.WriteLine($"domain: {eSTdata.Results.domain}");
+                Console.WriteLine($"capital: {eSTdata.Results.capital}");
+                Console.WriteLine($"region: {eSTdata.Results.region}");
+                Console.WriteLine($"population: {eSTdata.Results.population}");
+                Console.WriteLine($"language: {eSTdata.Results.language}");
+
+
+
+            }
+        }
+    }
+}
